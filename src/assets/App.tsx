73aac31c2474d69de3786349a4bbd866caedbdcd
Enzo Cerqueira <enzo.cerqueira42@gmail.com>
2025-06-09 16:40:42 -0300

    adding menu, count and form

--- src/assets/App.tsx
+++ src/assets/App.tsx
@@ -1,20 +1,46 @@
 import '../styles/theme.css';
 import '../styles/global.css';
 import { Container } from '../components/Container';
-import { Heading } from '../components/Heading';
+import { Logo } from '../components/Logo';
+import { Menu } from '../components/Menu';
+import { CountDown } from '../components/CountDown';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>ICON</Heading>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
-      
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form'>
+          <div className="formRow">
+            <label htmlFor="input">Task</label>
+            <input id='input' type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
